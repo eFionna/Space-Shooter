69feb7537f31b601feb1f4298c28d9abe44fd047
eFionna <56249078+eFionna@users.noreply.github.com>
2023-10-16 21:26:59 +0200

    Added Space object with relevant data

---
+++ Assets/Scripts/Components/SpaceProperties.cs
@@ -0,0 +1,15 @@
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Mathematics;
+
+public struct SpaceProperties : IComponentData
+{
+    public float2 Size;
+    public int AsteroidsToSpawn;
+}
+
+[InternalBufferCapacity(10)]
+public struct AstroidBuffer : IBufferElementData
+{
+    public Entity Value;
+}
---
+++ Assets/Scripts/Mono/SpaceMono.cs
@@ -0,0 +1,36 @@
+using Unity.Entities;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class SpaceMono : MonoBehaviour
+{
+    public float2 Size;
+    public int AsteroidsToSpawn;
+    public GameObject[] AstroidPrefabs;
+}
+
+public class SpaceBaker : Baker<SpaceMono>
+{
+    public override void Bake(SpaceMono authoring)
+    {
+        if (authoring.AstroidPrefabs.Length > 10)
+        {
+            Debug.LogWarning("AstroidPrefabs is bigger then 10. Will not Bake with bad data.");
+            return;
+        }
+        var entity = GetEntity(TransformUsageFlags.None);//Why???
+
+        var Buff = AddBuffer<AstroidBuffer>(entity);
+
+        for (int i = 0; i < authoring.AstroidPrefabs.Length; i++)
+        {
+            Buff.Add(new AstroidBuffer {Value = GetEntity(authoring.AstroidPrefabs[i], TransformUsageFlags.Dynamic) });
+        }
+
+        AddComponent(entity,new SpaceProperties
+        {
+            Size = authoring.Size,
+            AsteroidsToSpawn = authoring.AsteroidsToSpawn,
+        });
+    }
+}
