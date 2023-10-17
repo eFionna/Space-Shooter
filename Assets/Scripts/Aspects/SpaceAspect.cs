f38d2354b65e70b05aeeee71f8bd448d93f4cd63
eFionna <56249078+eFionna@users.noreply.github.com>
2023-10-17 12:12:13 +0200

    Spawning seeded random astriods

--- Assets/Scripts/Aspects/SpaceAspect.cs
+++ Assets/Scripts/Aspects/SpaceAspect.cs
@@ -7,15 +7,31 @@ public readonly partial struct SpaceAspect : IAspect
 {
     public readonly Entity entity;
 
-    private readonly RefRW<LocalTransform> transform;
+    private readonly RefRO<LocalTransform> transform;
     [ReadOnly] private readonly DynamicBuffer<AstroidBuffer> astroidPrefabBuffer;
     private readonly RefRO<SpaceProperties> spaceProperties;
     private readonly RefRW<SpaceRandom> spaceRandom;
 
+    public float3 Position => transform.ValueRO.Position;
 
-    public float3 Position
+    public int AsteroidsToSpawn => spaceProperties.ValueRO.AsteroidsToSpawn;
+
+    public Entity GetRandomAstroidPrefab()
     {
-        get => transform.ValueRO.Position;
-        set => transform.ValueRW.Position = value;
+        int randomVal = spaceRandom.ValueRW.Random.NextInt(astroidPrefabBuffer.Length);
+        return astroidPrefabBuffer[randomVal].Value;
     }
+
+    public quaternion GetRandomRotation() => quaternion.RotateZ(spaceRandom.ValueRW.Random.NextFloat(360.0f));
+    public float3 GetRandomPosition() => spaceRandom.ValueRW.Random.NextFloat3(MinCorner, MaxCorner);
+
+
+    private float3 MinCorner => transform.ValueRO.Position - HalfDimensions;
+    private float3 MaxCorner => transform.ValueRO.Position + HalfDimensions;
+    private float3 HalfDimensions => new()
+    {
+        x = spaceProperties.ValueRO.Size.x * 0.5f,
+        y = spaceProperties.ValueRO.Size.y * 0.5f,
+        z = 0
+    };
 }
--- Assets/Scripts/Systems/SpawnAstriodsSystem.cs
+++ Assets/Scripts/Systems/SpawnAstriodsSystem.cs
@@ -1,6 +1,8 @@
 using Unity.Burst;
 using Unity.Entities;
-using UnityEngine;
+using Unity.Collections;
+using Unity.Transforms;
+using Unity.Mathematics;
 
 [BurstCompile]
 public partial struct SpawnAstriodsSystem : ISystem
@@ -9,15 +11,26 @@ public partial struct SpawnAstriodsSystem : ISystem
     public void OnCreate(ref SystemState state)
     {
         state.RequireForUpdate<SpaceProperties>();
-
     }
+
     [BurstCompile]
     public void OnUpdate(ref SystemState state)
     {
         state.Enabled = false;
         var spaceEntity = SystemAPI.GetSingletonEntity<SpaceProperties>();
         SpaceAspect asp = SystemAPI.GetAspect<SpaceAspect>(spaceEntity);
-        Debug.Log(asp);
+
+        var ecb = new EntityCommandBuffer(Allocator.Temp);
+
+        for (int i = 0; i < asp.AsteroidsToSpawn; i++)
+        {
+            var astroid = ecb.Instantiate(asp.GetRandomAstroidPrefab());
+            var newTransform = asp.GetRandomPosition();
+            var rot = asp.GetRandomRotation();
+
+            ecb.SetComponent(astroid, new LocalTransform { Position = newTransform,Scale = 1.0f,Rotation = rot });
+        }
+        ecb.Playback(state.EntityManager);
     }
     [BurstCompile]
     public void OnDestroy(ref SystemState state)
